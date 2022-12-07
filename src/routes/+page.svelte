fd0b582d3b0d6e998a1824f7b07127ab71d450ef
Ali Alaa <hello@alialaa.com>
2022-12-07 12:16:56 +0200

    Code for organazing-components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
+<script>
+	import { Button } from '$components';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button />
 
 <a href="/about">About</a>
--- src/routes/product/[id]/+page.svelte
+++ src/routes/product/[id]/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	import { page } from '$app/stores';
+	import ProductHeader from './ProductHeader.svelte';
 
 	$: console.log($page);
 </script>
 
-<h1>Product Page</h1>
+<ProductHeader />
 
 <p>Product ID: {$page.params.id}</p>
 
