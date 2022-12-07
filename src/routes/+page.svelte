c66e78f03a1640d94564711653a21f7f19027406
Ali Alaa <hello@alialaa.com>
2022-12-07 12:45:37 +0200

    Code for navigation-functions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
 <script>
+	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
 	import { Button } from '$components';
+	import { navigating } from '$app/stores';
+
+	$: console.log($navigating);
+
+	beforeNavigate((navigation) => {
+		console.log('beforeNavigate', navigation);
+	});
+
+	afterNavigate((navigation) => {
+		console.log('afterNavigate', navigation);
+	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button
+	on:click={() => {
+		goto('/about');
+	}}>Go to About</button
+>
 
 <Button />
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,15 @@
 <script>
+	import { goto } from '$app/navigation';
+
 	let name = 'John';
 </script>
 
 <h1>About {name}</h1>
+
+<button
+	on:click={() => {
+		goto('/');
+	}}>Go to Home</button
+>
 
 <style></style>
