81f8284050bfdffd585eeb6dff71d0aa10560d45
Ali Alaa <hello@alialaa.com>
2022-12-07 13:37:53 +0200

    Code for optional-params

--- src/routes/[[lang]]/about/+page.svelte
+++ src/routes/[[lang]]/about/+page.svelte
@@ -1,10 +1,13 @@
 <script>
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 
 	let name = 'John';
 </script>
 
 <h1>About {name}</h1>
+
+{JSON.stringify($page.params)}
 
 <button
 	on:click={() => {
