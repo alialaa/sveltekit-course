2e341014e2b532515df6a6fcb24b7ad2bab41d30
Ali Alaa <hello@alialaa.com>
2022-12-08 17:21:54 +0200

    Code for layout-groups

---
+++ src/routes/(admin)/+layout.svelte
@@ -0,0 +1,6 @@
+<div style:background="#81d179" style:padding="10px">
+	<h3>(admin) group layout</h3>
+	Admin
+
+	<slot />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,7 @@
-<script>
-	import { page } from '$app/stores';
-</script>
+<div style:background="#f03" style:padding="10px">
+	<h3>Root Layout</h3>
 
-{#if $page.url.pathname.startsWith('/setting')}
 	<slot />
-{:else}
-	<div style:background="#f03" style:padding="10px">
-		<h3>Root Layout</h3>
-		<nav>
-			<a href="/">Home</a>
-			<a href="/about">About</a>
-			<a href="/settings">Settings</a>
-		</nav>
-		<slot />
 
-		<footer style:margin-top="60px">All rights reserved.</footer>
-	</div>
-{/if}
+	<footer style:margin-top="60px">All rights reserved.</footer>
+</div>
