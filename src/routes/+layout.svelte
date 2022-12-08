2422061833b8f76185ac3785c2f95442584199a7
Ali Alaa <hello@alialaa.com>
2022-12-08 16:57:03 +0200

    Code for layout-conditions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
-<div style:background="#f03" style:padding="10px">
-	<h3>Root Layout</h3>
-	<nav>
-		<a href="/">Home</a>
-		<a href="/about">About</a>
-		<a href="/settings">Settings</a>
-	</nav>
+<script>
+	import { page } from '$app/stores';
+</script>
 
+{#if $page.url.pathname.startsWith('/setting')}
 	<slot />
+{:else}
+	<div style:background="#f03" style:padding="10px">
+		<h3>Root Layout</h3>
+		<nav>
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/settings">Settings</a>
+		</nav>
+		<slot />
 
-	<footer style:margin-top="60px">All rights reserved.</footer>
-</div>
+		<footer style:margin-top="60px">All rights reserved.</footer>
+	</div>
+{/if}
