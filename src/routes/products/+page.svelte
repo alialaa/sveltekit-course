bd91fff71fcddd96188ded57874df80ef0909082
Ali Alaa <hello@alialaa.com>
2022-12-10 11:42:06 +0200

    Code for displaying-products

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -5,3 +5,31 @@
 
 	$: products = data.products.products;
 </script>
+
+<h2>Products</h2>
+
+{#if products && products.length > 0}
+	<ul>
+		{#each products as product}
+			<li>
+				<img src={product.thumbnail} alt={product.title} />
+				<h3><a href="/product/{product.id}">{product.title}</a></h3>
+				<p>{product.description}</p>
+			</li>
+		{/each}
+	</ul>
+{/if}
+
+<style>
+	ul {
+		padding: 0;
+		margin: 0;
+		list-style: none;
+		display: grid;
+		grid-template-columns: repeat(2, minmax(0, 1fr));
+		gap: 30px;
+	}
+	img {
+		width: 100%;
+	}
+</style>
