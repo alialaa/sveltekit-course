2dd60d245b018ca7f63ad1861134fd755da4eb4b
Ali Alaa <hello@alialaa.com>
2022-12-10 13:05:23 +0200

    Code for product-page

---
+++ src/routes/product/[id]/+page.server.ts
@@ -0,0 +1,16 @@
+import { error, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const products = await (await import('$lib/dummy-products.json')).default;
+	const product = products.products.find((p) => p.id === +params.id);
+
+	if (!product) {
+		// throw redirect(301, '/products');
+		throw error(404, 'Product not found!');
+	}
+
+	return {
+		product
+	};
+};
---
+++ src/routes/product/[id]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: product = data.product;
+</script>
+
+<img src={product?.thumbnail} alt={product?.title} />
+
+<h1>{product?.title}</h1>
+
+<p>{product?.description}</p>
+
+<style>
+	img {
+		width: 100%;
+	}
+</style>
