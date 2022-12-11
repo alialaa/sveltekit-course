fb3328323a2e47d67c53330f951e3d69127448c9
Ali Alaa <hello@alialaa.com>
2022-12-11 11:24:21 +0200

    Code for api-routes-2

--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -4,17 +4,17 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async () => {
 	const products = await (await import('$lib/dummy-products.json')).default;
 
-	throw error(401, 'Not authorized!');
-
-	// return new Response(JSON.stringify({ error: 'You are not authorized!' }), {
-	// 	status: 401
-	// });
-
 	return json(products, {
 		status: 200
 	});
+};
 
-	// return new Response(JSON.stringify(products), {
-	// 	status: 200
-	// });
+export const POST: RequestHandler = async ({ request }) => {
+	const product = await request.json();
+
+	if (!product.title) {
+		throw error(400, 'Product title is required!');
+	}
+
+	return json({ id: 1313, title: product.title });
 };
