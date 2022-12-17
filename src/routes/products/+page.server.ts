c401e6cb6a8054e12ce6d63fe832988bb9483f08
Ali Alaa <hello@alialaa.com>
2022-12-17 12:03:02 +0200

    Code for env-variables

--- src/routes/products/+page.server.ts
+++ src/routes/products/+page.server.ts
@@ -1,5 +1,21 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { env } from '$env/dynamic/private';
+import { env as publicEnv } from '$env/dynamic/public';
+import { API_KEY, TEST } from '$env/static/private';
+import { PUBLIC_KEY, PUBLIC_TEST } from '$env/static/public';
+
+// Evaluated dynamically at run time
+console.log('$env/dynamic/private from process.env', env.TEST);
+console.log('$env/dynamic/private from .env', env.API_KEY);
+console.log('$env/dynamic/public from process.env', publicEnv.PUBLIC_TEST);
+console.log('$env/dynamic/public from .env', publicEnv.PUBLIC_KEY);
+
+// Evaluated at build and injected in your code
+console.log('$env/static/private API_KEY', API_KEY);
+console.log('$env/static/private TEST', TEST);
+console.log('$env/static/public PUBLIC_KEY', PUBLIC_KEY);
+console.log('$env/static/public PUBLIC_TEST', PUBLIC_TEST);
 
 export const load: PageServerLoad = async ({ fetch, depends, locals }) => {
 	console.log('/products load function', locals);
--- src/routes/products/+page.ts
+++ src/routes/products/+page.ts
@@ -1,4 +1,9 @@
 import type { PageLoad } from './$types';
+import { env as publicEnv } from '$env/dynamic/public';
+import { PUBLIC_KEY, PUBLIC_TEST } from '$env/static/public';
+
+console.log(publicEnv);
+console.log(PUBLIC_KEY, PUBLIC_TEST);
 
 export const load: PageLoad = async ({ data, fetch, depends }) => {
 	fetch('https://dummyjson.com/products');
