4792aaf1cf72bd92ce46fe3243364083289cba5d
Ali Alaa <hello@alialaa.com>
2022-12-18 12:21:57 +0200

    Code for preloading-functions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
+<script lang="ts">
+	import { goto, preloadCode, preloadData } from '$app/navigation';
+</script>
+
 <h1>Home</h1>
+
+<button
+	on:focus={() => {}}
+	on:mouseover={async () => {
+		await preloadCode('/products');
+	}}
+	on:click={() => {
+		goto('/products', {
+			noScroll: true
+		});
+	}}
+	>Products
+</button>
