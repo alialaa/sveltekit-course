6dc6cd19f1300a572edc74679284a35649d58512
Ali Alaa <hello@alialaa.com>
2022-12-18 16:37:49 +0200

    Code for prerendering-5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 	import { goto, preloadCode, preloadData } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	let data: string[] = [];
+	onMount(() => {
+		setTimeout(() => {
+			data = ['Data 1', 'Data 2'];
+		}, 1000);
+	});
 </script>
 
 <h1>Home</h1>
+
+{#if data.length > 0}
+	<ul>
+		{#each data as item}
+			<li>{item}</li>
+		{/each}
+	</ul>
+{/if}
 
 <button
 	on:focus={() => {}}
