118fa386c57b39e9ad4ff42301fd83317f597bca
Ali Alaa <hello@alialaa.com>
2022-12-25 12:04:08 +0200

    heroku setup

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,6 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter(),
 		alias: {
