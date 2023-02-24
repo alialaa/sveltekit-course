b18f34c87b5acfb2e1c1e4ea337eeae1fe05d2cd
Ali Alaa <hello@alialaa.com>
2023-02-24 19:34:28 +0200

    snapshots example

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
 	import { goto, invalidateAll } from '$app/navigation';
+	import type { Snapshot } from './$types';
 
 	let username = '';
 	let password = '';
+
+	export const snapshot: Snapshot<{ username: string; password: string }> = {
+		capture: () => {
+			return {
+				username,
+				password
+			};
+		},
+		restore: (value) => {
+			username = value.username;
+			password = value.password;
+		}
+	};
 
 	const login = async () => {
 		const response = await fetch('/api/login', {
